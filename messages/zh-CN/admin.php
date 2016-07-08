4aabe258da5e792be8e3d1108534251bdc167c34
Simple Forum <flyyjd@gmail.com>
2016-07-08 13:20:30 +0900

    zh-CN language files

---
+++ messages/zh-CN/admin.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+/* setting/all */
+    'Forum Manager' => '论坛管理',
+    'Settings' => '配置管理',
+    'Nodes' => '节点管理',
+    'Navigation' => '导航管理',
+    'Members' => '用户管理',
+    'Links' => '链接管理',
+    'Test Email' => '邮件发送测试',
+    'Clear Cache' => '清空缓存',
+/* setting/clearCache */
+    'Cache is cleared.'=>'缓存清理完毕',
+    'SMTP Settings' => 'SMTP邮箱设置',
+    'An error has occurred with sending a test email. Please check {link}.' => '测试邮件发送出错，请确认 {link} 是否正确。',
+    'A test email has been successfully sent. Please check your email.' => '测试邮件发送成功，请进测试邮箱查看是否收到。',
+    'Test' => '测试',
+    'Email To' => '测试邮箱',
+    'Test Content' => '测试内容',
+
+    'Forum Info' => '论坛信息设置',
+    'Forum Manage' => '论坛管理设置',
+    'SMTP server' => 'SMTP服务器设置',
+    'Please {action} after saving SMTP server settings.' => '修改保存后，请 {action}',
+    'aaa' => '不用则留空，参考 <a href="http://simpleforum.org/t/2" target="_blank">开启第三方帐号登录</a>',
+    'QQ Login' => 'QQ登录设置',
+    'Weibo Login' => '微博登录设置',
+    'Cache' => '缓存设置',
+    'Upload' => '上传设置',
+    'Extend' => '扩展设置',
+    'Others' => '其它设置',
+/* link/index */
+    'Add a link' => '创建新链接',
+    'Edit a link' => '修改链接',
+/* link/_form */
+    'Edit a link' => '修改链接',
+/* navi */
+    'Add a item' => '创建导航项目',
+    'Edit a item' => '修改导航项目',
+    'Set Nodes' => '所属节点设定',
+    'Navigation Items' => '导航项目',
+    'Type' => '类型',
+    'Show' => '显示',
+/* node */
+    'Add a node' => '添加节点',
+    'Edit a node' => '修改节点',
+    'Search nodes' => '节点搜索',
+    'Node does not exist.' => '节点不存在。',
+/* user */
+    'Inactive' => '待激活用户',
+    'Verify' => '待验证用户',
+    'Banned' => '已屏蔽用户',
+    'Activate' => '激活',
+    'Member Info' => '用户信息',
+/* topic */
+    'Change Node'=>'转移主题节点',
+/* models/link */
+    'Sort ID'=>'排序',
+    'Link Text'=>'链接文字',
+    'Link Url'=>'链接网址',
+    'The results will be sorted from smallest to largest. Default value is {n}.' => '从小到大排序，默认{n}',
+/* models/navi */
+    'Item Name' => '项目名',
+    'Item ID' => '项目英文名',
+    'All nodes navi' => '所有节点页',
+    'Top navi' => '首页顶部',
+    'Bottom navi' => '首页底部',
+/* models/node */
+    'Node Name' => '节点名',
+    'Node ID' => '节点英文名',
+    'Topic Count' => '主题数',
+    'Topics only shown in this node\'s list' => '主题只显示在本节点',
+    'Description' => '描述',
+/* models/setting */
+    'Block' => '分类',
+    'Label' => '标签',
+    'Key' => '设置Key',
+    'Value' => '设置Value',
+];
